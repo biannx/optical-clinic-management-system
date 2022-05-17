f83c40c2a36929f457a8487d23ea55db6f032e30
yanggskie <lyahbianca.aquino04@gmail.com>
2022-05-18 02:13:53 +0800

    added customer tab UI

---
+++ assets/js/date.js
@@ -0,0 +1,51 @@
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+
+
+document.getElementById('dateAttendant').value = today;
+
+// upload profile picture
+const imgDiv= document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+// if user hover
+
+imgDiv.addEventListener('mouseenter', function()
+{
+    uploadBtn.style.opacity = "1"
+});
+
+// if we hover out from img div
+imgDiv.addEventListener('mouseleave', function()
+{
+    uploadBtn.style.opacity = "0.7" ; 
+});
+
+// showing image functionality when uploaded an image
+
+file.addEventListener('change', function()
+{
+    // refers to file
+    const choosedFile = this.files[0];
+    
+    if (choosedFile){
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+})
+
