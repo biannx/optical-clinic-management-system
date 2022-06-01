ebb532d406dc98e528d6bf35f8736655d176f8fc
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2022-06-01 23:07:05 +0800

    modified admin customer

--- assets/js/customers.js
+++ assets/js/customers.js
@@ -2,6 +2,7 @@
 const update= document.getElementById("update-button");
 
 update.addEventListener("click", function(){
+    document.getElementById("update-customer-photo").disabled=false;
     document.getElementById("view-fnameCustomer").disabled=false;
     document.getElementById("view-midnNameCustomer").disabled=false;
     document.getElementById("view-surnameCustomer").disabled=false;
@@ -14,6 +15,8 @@ update.addEventListener("click", function(){
     document.getElementById("view-contactCustomer").disabled=false;
 
     update.style.display="none"
+    document.getElementById('update-customer-photo-label').style.display = "block";
+    document.getElementById('update-customer-photo').style.display = "block";
     document.getElementById('save-update-button').style.display = "block";
     document.getElementById('cancel-update-button').style.display = "block";
 });
